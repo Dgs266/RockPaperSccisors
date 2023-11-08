c3493c0e698a6256634cb7f234d189607d3e856e
Dgs266 <150276602+Dgs266@users.noreply.github.com>
2023-11-08 10:36:23 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
 
+print('Welcome to my game of Rock,Paper,siccors!')
+Print('if you would like to play type yes')
+Input('Yes Or No ')
+if 'yes': 
+    print ('okay lets get started')
+else: 
+    print('okay, bye!')
